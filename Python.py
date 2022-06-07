6c0ccc88c0a8822733fc9e6f6be51a3e37d3ad5d
AndressaLCardoso <andressa_lira@outlook.com>
2022-06-07 08:57:46 -0300

    primeiro commit

---
+++ Python.py
@@ -0,0 +1,6 @@
+
+basemaior = int(input("Base maior = "))
+basemenor = int(input("Base menor ="))
+h = int(input("altura = "))
+a = (basemaior + basemenor)/2*h
+print("O valor da área do trapézio é de:",a)
